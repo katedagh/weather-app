1a8b046688cbe2cb400505b4112d66d1999c268a
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-07-14 19:40:08 +0200

    split js in more files

---
+++ api.js
@@ -0,0 +1,27 @@
+async function getCoordinates(city) {
+   const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`)
+
+   const data = await response.json()
+ 
+   const latitude = data.results[0].latitude
+   const longitude = data.results[0].longitude
+
+   return {latitude, longitude}
+}
+
+async function getWeather(latitude, longitude) {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,rain,uv_index,wind_speed_10m&current=temperature_2m,rain,cloud_cover,wind_speed_10m,apparent_temperature&timezone=auto`)
+
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        temperature: data.current.temperature_2m,
+        apparentTemperature: data.current.apparent_temperature,
+        sunrise: data.daily.sunrise[0].split("T")[1],
+        sunset: data.daily.sunset[0].split("T")[1],
+        rain: data.current.rain,
+        cloudCover: data.current.cloud_cover,
+        wind: data.current.wind_speed_10m
+    }
+}
