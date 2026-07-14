9d3e16ef8e9e8b1125b6767f67a0948276102fb2
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-07-14 19:06:26 +0200

    basic rendering of data on page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const searchForm = document.querySelector("#city-search")
 
+const weatherPage = document.querySelector("#weather")
+
 searchForm.addEventListener("submit", async event => {
     event.preventDefault()
 
@@ -10,7 +12,9 @@ searchForm.addEventListener("submit", async event => {
    
     const coordinates = await getCoordinates(city)
   
-    getWeather(coordinates.latitude, coordinates.longitude)
+    const weather = await getWeather(coordinates.latitude, coordinates.longitude)
+
+    renderData(weather, city)
 })
 
 
@@ -21,15 +25,39 @@ async function getCoordinates(city) {
  
    const latitude = data.results[0].latitude
    const longitude = data.results[0].longitude
-   
-   return { latitude, longitude}
+
+   return {latitude, longitude}
 }
 
-
-
 async function getWeather(latitude, longitude) {
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code`)
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,rain,uv_index,wind_speed_10m&current=temperature_2m,rain,cloud_cover,wind_speed_10m,apparent_temperature&timezone=auto`)
 
     const data = await response.json()
-    console.log(data) 
+    console.log(data)
+
+    return {
+        temperature: data.current.temperature_2m,
+        apparentTemperature: data.current.apparent_temperature,
+        sunrise: data.daily.sunrise[0].split("T")[1],
+        sunset: data.daily.sunset[0].split("T")[1],
+        rain: data.current.rain,
+        cloudCover: data.current.cloud_cover,
+        wind: data.current.wind_speed_10m
+    }
+}
+
+function renderData(weather, city) {
+    console.log(weather)
+   const card = document.createElement("p")
+
+   card.innerHTML = 
+   `City: ${city}<br> 
+    Temperature: ${weather.temperature}°C <br>
+    Apparent temperature: ${weather.apparentTemperature} °C <br>
+    Rain: ${weather.rain} <br>
+    Wind: ${weather.wind}m/s <br>
+    Sunrise: ${weather.sunrise} <br> 
+    Sunset: ${weather.sunset}`
+
+    weatherPage.appendChild(card)
 }
