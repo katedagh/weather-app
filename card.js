1ca821d8f15d91770f33a61dd47172068cc7f362
Kateřina Daghbouj <katerina.daghbouj@gmail.com>
2026-07-15 18:13:01 +0200

    add wind to weather card

--- card.js
+++ card.js
@@ -8,41 +8,55 @@ export function renderData(weather, city) {
     const card = document.createElement("div")
     card.classList.add("card")
    
-   const cityName = document.createElement("h2")
-   cityName.textContent = city.charAt(0).toUpperCase() + city.slice(1)
-   card.appendChild(cityName)
+    const cityName = document.createElement("h2")
+    cityName.textContent = city.charAt(0).toUpperCase() + city.slice(1)
+    card.appendChild(cityName)
 
-const icon = getWeatherIcon(weather.weatherCode) 
+    const icon = getWeatherIcon(weather.weatherCode) 
 
-const weatherIcon = document.createElement("div")
-weatherIcon.innerHTML = `${icon}`
-card.appendChild(weatherIcon)
+    const weatherIcon = document.createElement("div")
+    weatherIcon.innerHTML = `${icon}`
+    card.appendChild(weatherIcon)
   
-   const temperature = document.createElement("p")
-   temperature.classList.add("text-bold")
-  temperature.textContent = `${weather.temperature}°C`
-  card.appendChild(temperature)
+   const temperature = document.createElement("div")
 
-  const sunTime = document.createElement("div")
-  sunTime.classList.add("suntime")
-  sunTime.innerHTML = `
-  <div class="suntime-box">
+    const realTemperature = document.createElement("p")
+    realTemperature.classList.add("text-bold")
+    realTemperature.textContent = `${weather.temperature}°C`
+    const apparentTemperature = document.createElement("p")
+    apparentTemperature.classList.add("text-small")
+    apparentTemperature.textContent = `Feels like ${weather.apparentTemperature}°C`
+    temperature.appendChild(realTemperature)
+    temperature.appendChild(apparentTemperature)
+    card.appendChild(temperature)
+
+    const wind = document.createElement("div")
+    wind.classList.add("container-small")
+    wind.innerHTML = `<svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15.7639 7C16.3132 6.38625 17.1115 6 18 6C19.6569 6 21 7.34315 21 9C21 10.6569 19.6569 12 18 12H3M8.50926 4.66667C8.87548 4.2575 9.40767 4 10 4C11.1046 4 12 4.89543 12 6C12 7.10457 11.1046 8 10 8H3M11.5093 19.3333C11.8755 19.7425 12.4077 20 13 20C14.1046 20 15 19.1046 15 18C15 16.8954 14.1046 16 13 16H3" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg> 
+    <p>${weather.wind} m/s</p>`
+    card.appendChild(wind)
+
+    const sunTime = document.createElement("div")
+    sunTime.classList.add("container-small")
+    sunTime.innerHTML = `
+    <div class="suntime-box">
     <h3>Sunrise</h3>
     <p class="suntime-text">${weather.sunrise}</p>
     <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M12 10V3M12 3L9 6M12 3L15 6M6 12L5 11M18 12L19 11M3 18H21M5 21H19M7 18C7 15.2386 9.23858 13 12 13C14.7614 13 17 15.2386 17 18" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
-  </div>
+    </div>
  
- <div class="suntime-box">
+    <div class="suntime-box">
     <h3>Sunset</h3>
     <p class="suntime-text">${weather.sunset}</p> 
     <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M6 12L5 11M18 12L19 11M3 18H21M5 21H19M7 18C7 15.2386 9.23858 13 12 13C14.7614 13 17 15.2386 17 18M12 3V10M12 10L15 7M12 10L9 7" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
- </div>
-  `
-  card.appendChild(sunTime)
-
-weatherPage.appendChild(card)
+    </div>`
+    card.appendChild(sunTime)
+ 
+    weatherPage.appendChild(card)
 }
